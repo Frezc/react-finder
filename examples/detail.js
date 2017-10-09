50103a19882ca2c809e7a2225976072c6332de5a
frezc <504021398@qq.com>
2017-10-09 11:27:02 +0800

    Updates

---
+++ examples/detail.js
@@ -0,0 +1,19 @@
+webpackJsonp([2],{
+
+/***/ 377:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(378);
+
+
+/***/ }),
+
+/***/ 378:
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+
+},[377]);
+//# sourceMappingURL=detail.js.map
