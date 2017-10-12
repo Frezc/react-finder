29e9ab0844732aab9198ea14a1d236a4a068dbec
frezc <504021398@qq.com>
2017-10-12 14:36:00 +0800

    fix lint error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * Created by wanli on 2017/9/15.
  */
 import ReactFinder from './ReactFinder';
-import '../assets/styles.css';
+import '../assets/styles.scss';
 
 export { SortableElement, SortableContainer, SortableHandle, arrayMove } from 'react-sortable-hoc';
 export default ReactFinder;
